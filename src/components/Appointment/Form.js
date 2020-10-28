34b755e58ffbdbe62cda803b8c4cd8439906df48
jilliankmartin <jillmartin@live.com>
2020-10-27 17:43:36 -0700

    added state to the form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,28 +1,42 @@
-import React from "react";
+import React, { useState } from 'react'
 import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
 
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = function() {
+    setName(props.name || ""); 
+    setInterviewer(props.interviewer || null);
+  }
+
+  const cancel = function() {
+    reset();
+    props.onCancel();
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
+            value={name}
+            onChange={(event) => setName(event.target.value)}
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
-            placeholder={props.name || "Enter student name"} 
-            /*
-              This must be a controlled component
-            */
+            placeholder={props.name || "Enter student name"}
           />
         </form>
-        <InterviewerList interviewers={props.interviewers} interviewer={props.interviewer} onChange={props.onSave} />
+        <InterviewerList interviewers={props.interviewers} interviewer={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button onClick={props.onCancel} danger>Cancel</Button>
+          <Button onClick={cancel} 
+                  danger>Cancel
+          </Button>
           <Button onClick={props.onSave} confirm>Save</Button>
         </section>
       </section>
