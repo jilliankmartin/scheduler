806855f4b2b94af8bb9613420b4f8545cd6900ba
jilliankmartin <jillmartin@live.com>
2020-10-28 12:33:41 -0700

    connected API to retrieve days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 import "components/Application.scss";
+import axios from 'axios';
+
 
 const appointments = [
   {
@@ -42,27 +44,17 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 export default function Application(props) {
 
-    const [day, setDay] = useState("Monday")
+    const [days, setDays] = useState([])
+    const [day, setDay] = useState(["Monday"])
+
+    useEffect(() => {
+      axios
+        .get('/api/days')
+        .then((response) => {(setDays(response.data))})
+    }, [])
 
     const listMaker = appointments.map((appointment) => 
     <Appointment key={appointment.id} {...appointment} />
