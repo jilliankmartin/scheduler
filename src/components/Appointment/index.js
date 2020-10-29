01a5599659bdc8a20eeb9dcf81cd81269dab36b4
jilliankmartin <jillmartin@live.com>
2020-10-29 15:39:16 -0700

    added transitions to the form - empty create and show

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,21 +4,39 @@ import Show from "./Show";
 import Header from "./Header";
 import Empty from "./Empty";
 import useVisualMode from "hooks/useVisualMode";
+import Form from "components/Appointment/Form";
 
 export default function Appointment(props) {
 
-  const showOrEmpty = function() {
-    let code = props.interview ? 
-    <Show student={props.interview.student} interviewer={props.interview.interviewer}/> 
-    : 
-    <Empty onAdd={props.onAdd}/>
-    return code;
-  }
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  // const showOrEmpty = function() {
+  //   let code = props.interview ? 
+  //   <Show student={props.interview.student} interviewer={props.interview.interviewer}/> 
+  //   : 
+  //   <Empty onAdd={props.onAdd}/>
+  //   return code;
+  // }
 
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {showOrEmpty()}
+        {mode === EMPTY && 
+          <Empty onAdd={() => transition(CREATE)} />}
+        {mode === SHOW && (
+          <Show
+            student={props.interview.student}
+            interviewer={props.interview.interviewer} />
+        )}
+        {mode === CREATE && 
+          <Form interviewers={[]} onSave={() => {console.log("save")}} onCancel={back}/>
+        }
     </article>
   )
 }
