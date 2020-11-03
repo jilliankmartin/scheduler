b08c884a91c17310aa51ac082aaeaad716160cba
jilliankmartin <jillmartin@live.com>
2020-11-03 13:08:55 -0800

    refactored updatespots to be less crap

--- src/helpers/updateSpots.js
+++ src/helpers/updateSpots.js
@@ -1,7 +1,7 @@
 
   //takes in a number of open spots for the day and returns a new days object to use to update state
   export default function updateEmptySpots(state, id) {
-    const openSpotCount = countSpots(state);
+    const openSpotCount = countSpots(state, id);
     const days = [...state.days]
     for (const key in days) {
       if (days[key].appointments.includes(id)) {
@@ -13,32 +13,19 @@
   }
 
   //return a count of the number of open spots in the appointments for the relevant day
-  const countSpots = function(state) {
+  const countSpots = function(state, id) {
     const appointments = state.appointments
-    const index = getKeyForDay(state.day)
+    const days = state.days
     let count = 0;
-      for (let item in appointments) {
-        for (let i = index; i < index + 5; i++) {
-          // eslint-disable-next-line
-            if (item == i && !appointments[item].interview) {
+      for (let i = 0; i < days.length; i++) {
+        if (days[i].appointments.includes(id)) {
+          for (let key in days[i].appointments) 
+            if (appointments[days[i].appointments[key]].interview === null) {
               count += 1;
             }
         }
       }
     return count;
   }
-
-  
-  //returns the correct index to start counting appointments at given the day supplied in the state
-  const getKeyForDay = function(day) {
-    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-    const indexes = [1, 6, 11, 16, 21]
-    for (let i = 0; i < days.length; i++) {
-      if (day === days[i]) {
-        return indexes[i]
-      }
-    }
-  }
-  
 
 
